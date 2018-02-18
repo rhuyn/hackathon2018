c5491e60cf4ef32d9710eb98fe5756632a3edf93
Austin Wattling <awattling@gmail.com>
2018-02-18 11:12:25 -0700

    adding pictures

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router:Router) {
+  }
 
   ngOnInit() {
   }
-  //    this._router.navigateByUrl('/about');
+  aboutclicked() {
+    this._router.navigateByUrl('/about');
+  }
 }
